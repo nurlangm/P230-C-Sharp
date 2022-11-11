d7652c96f6e8c10ed6de7820126da44c11c7f120
unknown <you@example.com>
2022-11-11 19:42:33 +0400

    First C# Task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿
+#region 2nin quvveti olub olmamasi
+//int n = 8;
+//if (n % 2 == 0)
+//{
+//    while (n % 2 == 0)
+//    {
+//        n /= 2;
+//    }
+//    if (n == 1)
+//    {
+//        Console.WriteLine("Quvvetidir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Quvveti deyil");
+//    }
+
+//}
+//else
+//{
+//    if (n == 1)
+//    {
+//        Console.WriteLine("Quvvetidir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Quvveti deyil");
+//    }
+//} 
+#endregion
+
+#region Arasindaki ededler
+//Console.WriteLine("Birinci ededi girin:");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ikinci ededi girin:");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-----------------------");
+
+
+//for (int i = num1; i < num2; i++)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+
+#region 70den kicik boyuk
+//Console.Write("Eded Daxil edin:");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = 70;
+
+//if (num1 > num2)
+//{
+//    num1 -= num2;
+//    num1 = num1 * 3;
+//    Console.WriteLine(num1);
+
+//}
+
+//else
+//{
+//    num2 -= num1;
+//    Console.WriteLine(num2);
+//}
+
+#endregion
+
+#region X,Y,Z
+//Console.WriteLine("X:");
+//Console.WriteLine("-----");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-----");
+//Console.WriteLine("Y:");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-----");
+//Console.WriteLine("Z:");
+//int z = Convert.ToInt32(Console.ReadLine());
+//if (x == y & x == z) //Duzdu sertde bele birsey yoxdu elebele yazdim Muellim 
+//{
+//    Console.WriteLine("Her biri beraberdir");
+//}
+
+//else if (x > y && x > z)
+//{
+//    Console.WriteLine("X en boyukdur");
+//}
+//else if (y > z && y > x)
+//{
+//    Console.WriteLine("Y en boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("Z en boyukdur");
+//} 
+#endregion
+
+#region Iki eded arasindaki tek ededlerin sayi
+//Console.WriteLine("Birinci ededi girin:");
+//int N = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ikinci ededi girin:");
+//int M = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-----------------------");
+//int result = 0;
+
+
+//for (int i = N; i < M; i++)
+//{
+//	if (i % 2 == 1)
+//	{
+//		result++;
+//	}
+//}
+//Console.WriteLine(result); 
+#endregion
